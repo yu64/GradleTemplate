6ff961aa3c20fb0bf57973aa4493daf3ab82af42
yu64 <yumaikuto1212@gmail.com>
2022-07-30 11:04:51 +0900

    .projectを変更不可に設定。bat追加

---
+++ bat/git_assume_unchanged_project.bat
@@ -0,0 +1,11 @@
+@echo off
+
+for /F "usebackq" %%i in (`git ls-files`) do (
+
+    echo %%i | findstr ".project" > NUL
+    if not ERRORLEVEL 1 (
+        
+        echo %%i
+        git update-index --assume-unchanged %%i
+    )
+)
