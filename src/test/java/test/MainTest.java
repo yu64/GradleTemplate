f85f68656f40bf72ef18dab77e3eda5fa5f947c7
yu64 <yumaikuto1212@gmail.com>
2022-10-16 05:21:57 +0900

    buildSrcを使用していた時のサンプルコードを復元

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -1,6 +1,10 @@
 package test;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+
+import test.symbols.SymbolTestSymbols;
 
 
 public class MainTest {
@@ -9,6 +13,7 @@ public class MainTest {
 	@Test
 	public void test()
 	{
+		Arrays.stream(SymbolTestSymbols.ATTRIBUTES).forEach(System.out::println);
 		
 	}
 
---
+++ src/test/java/test/SymbolTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import one.equinox.symbols.Symbolize;
+
+@Symbolize
+public class SymbolTest {
+
+    private String value;
+
+    public String getValue()
+    {
+        return this.value;
+    }
+}
