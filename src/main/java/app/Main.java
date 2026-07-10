8158afc33682aa6ccc84af8ccbdccad0bf6dc9d8
yu64 <yumaikuto1212@gmail.com>
2026-07-11 03:09:17 +0900

    java25に移行

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,12 @@
 package app;
 
-public class Main 
+import java.lang.IO;
+
+public class Main
 {
     public static void main(String[] args) 
     {
         System.out.println("Hello World (ハローワールド)");
+        IO.println("Hello World (ハローワールド)");
     }
 }
